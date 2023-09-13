f03b290d512bc2ca16d43b086639f6e606aec224
bbungsang94 <bbubgsang94@ajou.ac.kr>
2023-09-13 15:18:19 +0900

    testing diff

--- dummy.py
+++ dummy.py
@@ -13,21 +13,23 @@ def main():
     diffusion = GaussianDiffusion(
         model,
         image_size=256,
-        timesteps=1000,  # number of steps
-        sampling_timesteps=250
+        timesteps=100,  # number of steps
+        sampling_timesteps=25
         # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
     )
 
     trainer = Trainer(
         diffusion,
         r'D:\Creadto\Heritage\Dataset\skin-tester',
-        train_batch_size=16,
+        train_batch_size=8,
         train_lr=8e-5,
-        train_num_steps=700000,  # total training steps
+        train_num_steps=2500,  # total training steps
         gradient_accumulate_every=2,  # gradient accumulation steps
         ema_decay=0.995,  # exponential moving average decay
         amp=True,  # turn on mixed precision
-        calculate_fid=True  # whether to calculate fid during training
+        calculate_fid=False,  # whether to calculate fid during training
+        save_best_and_latest_only=False,
+        save_and_sample_every=124
     )
 
     trainer.train()
